8435a77a0cf862aaf4b1d09e8e6defb518ad3c8f
anamahalo <57977136+anamahalo@users.noreply.github.com>
2020-08-18 10:46:39 -0500

    Make this == operation caveat more verbose.

--- topics/go/language/maps/notes/syntax.go
+++ topics/go/language/maps/notes/syntax.go
@@ -7,5 +7,6 @@
 // Iterate:    for k, v := range m
 // Size:       len(m)
 
-// Key must have an == operation (no maps, slices, or funcs as keys)
+// Key must have an == operation (i.e.: no maps, slices, or funcs as keys)
+// (Value type can be anything)
 // Operations run in constant expected time.
