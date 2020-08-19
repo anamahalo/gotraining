30656d95334e81959bfaac5d5281300fd727c110
anamahalo <57977136+anamahalo@users.noreply.github.com>
2020-08-19 18:04:52 -0500

    Embedding notes.

---
+++ topics/go/language/embedding/notes/notes.go
@@ -0,0 +1,13 @@
+// Embedding
+// Final piece of sharing and reusing state and behavior between types
+// Thru use of inner type promotion, an inner type's fields
+// and methods can be directly accessed by references
+// of the outer type
+
+//  Embedding types allows us to share state or behavior
+//  between types
+//  Inner type loses its identity
+//  Not inheritance
+//  Through promotion, inner type fields and methods
+//  can be accessed through the outer type.
+//  The outer type can override the inner type's behavior
