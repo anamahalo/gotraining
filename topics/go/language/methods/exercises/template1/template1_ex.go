df2d386d665b73b8eafef8cd8566a9bf02613bb8
anamahalo <57977136+anamahalo@users.noreply.github.com>
2020-08-19 00:23:12 -0500

    Add imports, declare player struct.

---
+++ topics/go/language/methods/exercises/template1/template1_ex.go
@@ -0,0 +1,29 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Declare a struct that represents a baseball player. Include name, atBats and hits.
+// Declare a method that calculates a player's batting average. The formula is hits / atBats.
+// Declare a slice of this type and initialize the slice with several players. Iterate over
+// the slice displaying the players name and batting average.
+package main
+
+// Add imports.
+import "fmt"
+// Declare a struct that represents a ball player.
+// Include fields called name, atBats and hits.
+type player struct {
+    name   string
+    atBats int
+    hits   int
+}
+// Declare a method that calculates the batting average for a player.
+func ( /* receiver */ ) average() /* return type */ {
+}
+
+func main() {
+
+	// Create a slice of players and populate each player
+	// with field values.
+
+	// Display the batting average for each player in the slice.
+}
