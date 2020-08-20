6824c3324e0586b9940015ebabba818bfb8e4883
anamahalo <57977136+anamahalo@users.noreply.github.com>
2020-08-20 16:53:17 -0500

    Add notes about rethinking bugs not as bugs but as failed measures of predictability, with resolutions.

---
+++ topics/go/design/error_handling/notes/bugs.go
@@ -0,0 +1,20 @@
+// Bugs are NOT flaws in code, but rather
+// a FAILURE TO PREDICT what that code would do
+// .: We shouldn't focus on the broken code,
+//    but on improving our predictions.
+
+// So how do we improve our predictions?
+//  Tests
+//      Writing them, and actual usability
+//  Complexity
+//      K.I.S.S., otherwise taxes our mental models as humans
+//  Playing computer
+//      Don't predict; execute
+//      (i.e.: let the compiler tell you, or execute rather than guessing)
+//  "Readability" - actually PREDICTABILITY
+//      Prediction is more likely when a piece of code LOOKS like what it does
+//      Best measure: how often would a programmer NEW TO THIS CODE make CORRECT
+//          predictions about how it behaves?
+
+// readability isn't just about reducing overhead but also working to mitigate
+//  errors and debunking assumptions
